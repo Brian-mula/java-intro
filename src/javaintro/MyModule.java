39bbd0ef30529c6a634ebdf672aa7d1e6b1a50c8
Brian-mula <mulatiwangilabrian@gmail.com>
2021-07-26 11:02:45 +0300

    Initial commit

---
+++ src/javaintro/MyModule.java
@@ -0,0 +1,21 @@
+package javaintro;
+
+import java.util.Date;
+
+public class MyModule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=30;
+		int herAge=age+1;
+		System.out.println("Hello world");
+		System.out.println(age);
+		System.out.println(herAge);
+		
+		Date now=new Date();
+		System.out.println(now);
+		
+		
+	}
+
+}
